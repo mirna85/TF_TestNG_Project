d5016cc289936b25a66f29b652ebf603dc64933d
mirna85 <77590443+mirna85@users.noreply.github.com>
2021-06-21 23:17:54 -0500

    4th commit

--- src/main/java/variousConcepts/GitTest_testBranch.java
+++ src/main/java/variousConcepts/GitTest_testBranch.java
@@ -1,5 +1,19 @@
 package variousConcepts;
 
 public class GitTest_testBranch {
+	
+public static void main(String[] args) {
+		
+		printMe();
+		
+	}
+	
+	public static void printMe() {
+		
+		System.out.println("git test");
+		
+	}
+	
+	
 
 }
